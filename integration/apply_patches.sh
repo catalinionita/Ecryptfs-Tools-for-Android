18f379bd664d05510b15a8e79122184a987e94f1
Nicolae-Alexandru Ivan <alexandru.ivan@intel.com>
2014-11-13 14:37:35 +0200

    Fix comparison bug in apply_patches.sh

--- integration/apply_patches.sh
+++ integration/apply_patches.sh
@@ -18,7 +18,7 @@ git apply "$basepath/$version/vold/Vold-integration.patch" --directory=system/vo
 # Core integration
 git apply "$basepath/$version/core/Core-integration.patch" --directory=system/core/
 # Framework integration
-if [ $version="android-4.4_r1.2" ]; then
+if [ $version = "android-4.4_r1.2" ]; then
     git apply "$basepath/$version/frameworks-base/MountManagerService-integration.patch" --directory=frameworks/base/
 else
     git apply "$basepath/$version/frameworks-base/frameworks-base-integration.patch" --directory=frameworks/base/
