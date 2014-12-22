0a5865bc0493dde4a35160894601f3f1f82dbabc
Nicolae-Alexandru Ivan <alexandru.ivan@intel.com>
2014-12-22 16:38:34 +0200

    Android 5.0.0_r7 integration
    Only integrated as far as Vold, so only the vdc tests will pass.
    SeLinux policy has temporarily been bipased by commenting the
    "never allow" rules added in 5.0.0_r7, but proper rules need to
    be written.

--- integration/apply_patches.sh
+++ integration/apply_patches.sh
@@ -16,14 +16,19 @@ cp "$basepath/$version/vold/EncryptedFileStorageCmd.cpp" system/vold/
 cp "$basepath/$version/vold/EncryptedFileStorageCmd.h" system/vold/
 git apply "$basepath/$version/vold/Vold-integration.patch" --directory=system/vold/
 # Core integration
-git apply "$basepath/$version/core/Core-integration.patch" --directory=system/core/
+if [ $version != "android-5.0.0_r7" ]; then
+    git apply "$basepath/$version/core/Core-integration.patch" --directory=system/core/
+fi 
 # Framework integration
-if [ $version = "android-4.4_r1.2" ]; then
-    git apply "$basepath/$version/frameworks-base/MountManagerService-integration.patch" --directory=frameworks/base/
-else
-    git apply "$basepath/$version/frameworks-base/frameworks-base-integration.patch" --directory=frameworks/base/
-fi
+case $version in
+    "android-4.4_r1.2" )
+        git apply "$basepath/$version/frameworks-base/MountManagerService-integration.patch" --directory=frameworks/base/ ;;
+    "android-4.4.4_r2" )
+        git apply "$basepath/$version/frameworks-base/frameworks-base-integration.patch" --directory=frameworks/base/ ;;
+esac
 # Settings integration
-git apply "$basepath/$version/settings/Settings-integration.patch" --directory=packages/apps/Settings/
+if [ $version != "android-5.0.0_r7" ]; then
+    git apply "$basepath/$version/settings/Settings-integration.patch" --directory=packages/apps/Settings/
+fi
 # SeLinux policy integration
 git apply "$basepath/$version/sepolicy/EFS-SeLinux-Policy.patch" --directory=external/sepolicy/
